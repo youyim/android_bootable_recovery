b87d25b62e2053da36717bbf761aa4919f5e66a7
youyim123 <youyim@hotmail.com>
2019-04-14 13:12:13 +0800

    add author infomation

--- Android.mk
+++ Android.mk
@@ -398,6 +398,8 @@ endif
 ifneq ($(TW_CLOCK_OFFSET),)
 	LOCAL_CFLAGS += -DTW_CLOCK_OFFSET=$(TW_CLOCK_OFFSET)
 endif
+RECOVERY_BUILD_DATE := $(shell date +"%Y%m%d")
+LOCAL_CFLAGS += -DRECOVERY_BUILD_DATE="$(RECOVERY_BUILD_DATE)"
 LOCAL_REQUIRED_MODULES += \
     dump_image \
     erase_image \
